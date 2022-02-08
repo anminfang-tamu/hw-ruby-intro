d38e2365f6fdfd44c477ee084ecd7043709ab29b
Ubuntu <ubuntu@ip-172-31-17-175.ec2.internal>
2022-02-08 03:49:47 +0000

    Finished part-1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,14 +4,39 @@
 
 def sum arr
   # YOUR CODE HERE
+  arr.sum
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  else
+    arr.sort!
+    return arr[arr.length - 1] + arr[arr.length - 2]
+  end
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  arr.sort!
+  left = 0
+  right = arr.length - 1
+  
+  while left < right do
+    sum = arr[left] + arr[right]
+    if sum == n
+      return true
+    elsif sum > n
+      right = right - 1
+    else
+      left = left + 1
+    end
+  end
+  
+  return false
 end
 
 # Part 2
